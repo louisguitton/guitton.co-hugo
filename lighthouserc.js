ea80dcc3885609e42f0f681a66b9a63207494451
Louis Guitton <louisguitton93@gmail.com>
2021-05-23 18:06:45 +0200

    Add assertions

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,10 +1,39 @@
+// https://github.com/GoogleChrome/lighthouse-ci/blob/main/docs/configuration.md
 module.exports = {
   ci: {
     collect: {
-      staticDistDir: "./public",
+      startServerCommand: 'hugo server',
+      url: [
+        "http://localhost:1313/",
+        "http://localhost:1313/about",
+        "http://localhost:1313/posts",
+        "http://localhost:1313/contact",
+        "http://localhost:1313/posts/fastapi-monitoring/"
+      ],
     },
     upload: {
       target: "temporary-public-storage",
+    },
+    assert: {
+      preset: 'lighthouse:no-pwa',
+      assertions: {
+        'font-display': 'off',
+        'unminified-css': 'off',
+        'unsized-images': 'off',
+        'unused-css-rules': 'off',
+        'uses-text-compression': 'off',
+        'unused-javascript': 'off',
+        'uses-rel-preconnect': 'off',
+        'uses-responsive-images': 'off',
+        'image-alt': 'off',
+        'link-name': 'off',
+        'offscreen-images': 'off',
+        'total-byte-weight': 'off',
+        'uses-optimized-images': 'off',
+        'no-vulnerable-libraries': 'off',
+        'color-contrast': 'off',
+        'tap-targets': 'off',
+      },
     },
   },
 };

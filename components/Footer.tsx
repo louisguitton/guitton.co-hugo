9429378e0fdd73b1d040984f580e1b66bcf5f055
Louis Guitton <louisguitton93@gmail.com>
2022-01-01 16:49:25 +0100

    add better footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,45 +1,89 @@
+// Ref: https://devdojo.com/tails/app#_ Footer 3
+
+const navigation = {
+  site: [
+    { name: "Home", href: "/" },
+    { name: "About", href: "/about" },
+    { name: "Resume", href: "/resume" },
+    { name: "Freelance", href: "/freelance" },
+    { name: "Blog", href: "/posts" },
+  ],
+  social: [
+    { name: "Twitter", href: "https://twitter.com/louis_guitton" },
+    { name: "Github", href: "https://github.com/louisguitton" },
+    {
+      name: "Discord",
+      href: "https://discordapp.com/users/217929937842208768",
+    },
+  ],
+  other: [
+    { name: "Contact", href: "/contact" },
+    { name: "Awesome Tutos", href: "#" },
+    { name: "Feed", href: "#" },
+    { name: "2055", href: "#" },
+  ],
+};
+
 const Footer: React.FC = () => {
   return (
-    <footer className="text-gray-700 bg-white body-font">
-      <div className="container flex flex-col items-center px-8 py-8 mx-auto max-w-7xl sm:flex-row">
-        <a
-          href="/"
-          className="text-xl font-black leading-none text-gray-900 select-none logo">
-          guitton<span className="text-indigo-600">.</span>co
-        </a>
-        <p className="mt-4 text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l sm:border-gray-200 sm:mt-0">
-          © 2021 Louis Guitton
-        </p>
-        <span className="inline-flex justify-center mt-4 space-x-5 sm:ml-auto sm:mt-0 sm:justify-start">
-          <a
-            href="https://twitter.com/louis_guitton"
-            className="text-gray-400 hover:text-gray-500">
-            <span className="sr-only">Twitter</span>
-            <svg
-              className="w-6 h-6"
-              fill="currentColor"
-              viewBox="0 0 24 24"
-              aria-hidden="true">
-              <path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84"></path>
-            </svg>
-          </a>
-
-          <a
-            href="https://github.com/louisguitton"
-            className="text-gray-400 hover:text-gray-500">
-            <span className="sr-only">GitHub</span>
-            <svg
-              className="w-6 h-6"
-              fill="currentColor"
-              viewBox="0 0 24 24"
-              aria-hidden="true">
-              <path
-                fillRule="evenodd"
-                d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z"
-                clipRule="evenodd"></path>
-            </svg>
-          </a>
-        </span>
+    <footer className="w-full bg-white" aria-labelledby="footer-heading">
+      <div className="px-8 py-12 mx-auto max-w-7xl">
+        <div className="grid grid-cols-2 gap-10 mb-3 md:grid-cols-3 lg:grid-cols-12 lg:gap-20">
+          <div className="col-span-3">
+            {/* <a
+              href="#_"
+              className="text-xl font-black leading-none text-gray-900 select-none logo">
+              guitton.co
+            </a> */}
+            <p className="text-sm leading-normal text-gray-500">
+              Made in Berlin 🇩🇪 with NextJS, TailwindCSS, and Vercel.
+            </p>
+          </div>
+          <nav className="col-span-1 md:col-span-1 lg:col-span-2">
+            <p className="mb-3 text-xs font-semibold tracking-wider text-gray-400 uppercase">
+              Site
+            </p>
+            {navigation.site.map((item) => (
+              <a
+                key={item.name}
+                href={item.href}
+                className="flex mb-3 text-sm font-medium text-gray-500 transition hover:text-gray-700 md:mb-2 hover:text-primary">
+                {item.name}
+              </a>
+            ))}
+          </nav>
+          <nav className="col-span-1 md:col-span-1 lg:col-span-2">
+            <p className="mb-3 text-xs font-semibold tracking-wider text-gray-400 uppercase">
+              Social
+            </p>
+            {navigation.social.map((item) => (
+              <a
+                key={item.name}
+                href={item.href}
+                className="flex mb-3 text-sm font-medium text-gray-500 transition hover:text-gray-700 md:mb-2 hover:text-primary">
+                {item.name}
+              </a>
+            ))}
+          </nav>
+          <nav className="col-span-1 md:col-span-1 lg:col-span-2">
+            <p className="mb-3 text-xs font-semibold tracking-wider text-gray-400 uppercase">
+              Other
+            </p>
+            {navigation.other.map((item) => (
+              <a
+                key={item.name}
+                href={item.href}
+                className="flex mb-3 text-sm font-medium text-gray-500 transition hover:text-gray-700 md:mb-2 hover:text-primary">
+                {item.name}
+              </a>
+            ))}
+          </nav>
+          <div className="col-span-3">
+            <p className="mb-6 text-sm text-right text-gray-600 md:mb-0">
+              © Copyright 2022 Louis Guitton. All Rights Reserved.
+            </p>
+          </div>
+        </div>
       </div>
     </footer>
   );

ee860fa733e0827e376557f4fecffc0d2de51f0f
Louis Guitton <louisguitton93@gmail.com>
2022-01-03 20:21:46 +0100

    update SEO details

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,15 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Page from "../components/Page";
 
+const seoConfig = {
+  title: "guitton.co - personal website of Louis Guitton, not Louis Vuitton",
+  description:
+    "My personal serverless website where I blog and store useful snippets for python software development, machine learning, data engineering, and flow blockchain development.",
+  baseUrl: "https://guitton.co",
+  image: "/images/louis.jpg",
+};
+
+// TODO: fix canonical URL
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
@@ -12,36 +21,53 @@ function MyApp({ Component, pageProps }: AppProps) {
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
 
         {/* Ref: https://css-tricks.com/svg-favicons-and-all-the-fun-things-we-can-do-with-them/ */}
-        <link rel="icon" type="image/svg+xml" href="/favicon.svg"/>
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
       </Head>
 
       {/* Ref: https://github.com/garmeeh/next-seo#add-seo-to-page */}
       <NextSeo
-        title="guitton.co"
-        description="Welcome to the club. Your exclusive NFT of the brand new OFC Jersey 2022."
+        title={seoConfig.title}
+        description={seoConfig.description}
+        canonical={seoConfig.baseUrl}
+        // TODO: check if type = profile
         openGraph={{
-          url: "",
-          title: "guitton.co",
-          description:
-            "Welcome to the club. Your exclusive NFT of the brand new OFC Jersey 2022.",
+          url: seoConfig.baseUrl,
+          title: seoConfig.title,
+          description: seoConfig.description,
           type: "website",
           locale: "en_IE",
           images: [
             {
-              url: "/Composite_OFxOFC.png",
-              width: 600,
-              height: 600,
-              alt: "OneFootball Club",
-              type: "image/png",
+              url: seoConfig.image,
+              width: 224,
+              height: 224,
+              alt: "Louis Guitton",
+              type: "image/jpg",
             },
           ],
           site_name: "guitton.co",
+          profile: {
+            firstName: "Louis",
+            lastName: "Guitton",
+            gender: "male",
+          },
         }}
         twitter={{
-          handle: "@OneFootball",
-          site: "@OneFootball",
+          handle: "@louis_guitton",
+          site: "",
           cardType: "summary_large_image",
         }}
+        additionalMetaTags={[
+          {
+            name: "keywords",
+            content:
+              "blog,nlp,machine learning,data engineering,python,blockchain",
+          },
+          { name: "twitter:title", content: seoConfig.title },
+          { name: "twitter:description", content: seoConfig.description },
+          { name: "twitter:image", content: seoConfig.image },
+          // TODO: add "og:updated_time" ?
+        ]}
       />
 
       <Page>

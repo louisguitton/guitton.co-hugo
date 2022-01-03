fbde5c3bdf18ade5dc97963fd5559f64e92ccbba
Louis Guitton <louisguitton93@gmail.com>
2022-01-03 20:21:23 +0100

    small fix hero

--- README.md
+++ README.md
@@ -15,6 +15,7 @@ Next + markdown
 
 ## TODO
 
+- [ ] add jsonschema for SEO
 - [ ] build OK reading experience for the body using either markdown plugins or custom CSS
   - https://github.com/leerob/leerob.io/blob/main/components/Container.tsx#L119-L122
   - https://github.com/leerob/leerob.io/blob/main/layouts/blog.tsx#L53
@@ -26,3 +27,7 @@ Next + markdown
 - [ ] add SSG support for tweets
 
 - [ ] add search page for the lolz https://docs.google.com/presentation/d/1Iqb4rz1txVASXHpdjwogbiDvetVkjI9GMa7lQoIh7j4/edit#slide=id.g10a562423c5_0_4
+- [ ] add mentions page that combines:
+  - [ ] https://www.reddit.com/search/?q=site%3Aguitton.co%20OR%20author%3Alaguitte%20OR%20selftext%3A%22guitton.co%22&sort=new
+  - [ ] https://news.ycombinator.com/from?site=guitton.co
+  - [ ] https://twitter.com/search?q=%22guitton.co%22%20OR%20%22louis%20guitton%22%20-from%3Alouis_guitton%20-%40louis_guitton%20-bag%20-purse%20-from%3Alouisguitton&src=typed_query&f=live
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -3,46 +3,46 @@ import Image from "next/image";
 
 function Hero() {
   return (
-    <>
-      <section className="sm:grid sm:grid-cols-12 sm:gap-8">
-        <div className="sm:col-span-6">
-          <h1>
-            <span
-              className={clsx(
-                "flex mt-1",
-                "text-4xl sm:text-5xl",
-                "font-extrabold tracking-tight"
-              )}>
-              <span className="block text-gray-900">Louis&nbsp;</span>
-              <span className="inline-block text-primary-600">Guitton</span>
-            </span>
-            <span className="block text-sm font-semibold text-gray-500">
-              Tech Lead NFT and Principal Machine Learning Engineer at{" "}
-              <b>OneFootball</b>
-            </span>
-          </h1>
-          <p className="mt-3 text-sm text-gray-500 sm:mt-5">
-            Building for myself first, for fun and learnings. Experienced with
-            Natural Language Processing, Recommenders Systems, MVP Building and
-            the Flow blockchain.
-          </p>
-        </div>
-        <button
-          className={clsx(
-            "relative mt-12 sm:mt-0 sm:col-span-6 sm:flex sm:items-center",
-            "rounded-full shadow-lg h-56 w-56",
-            "mx-auto",
-            "focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary-500"
-          )}>
-          <Image
-            layout="fill"
-            src="https://guitton.co/images/louis.jpg"
-            className="rounded-full"
-            alt="Profile picture"
-          />
-        </button>
-      </section>
-    </>
+    <section className="sm:grid sm:grid-cols-12 sm:gap-8">
+      <div className="sm:col-span-6">
+        <h1>
+          <span
+            className={clsx(
+              "flex mt-1",
+              "text-4xl sm:text-5xl",
+              "font-extrabold tracking-tight"
+            )}>
+            <span className="block text-gray-900">Louis&nbsp;</span>
+            <span className="inline-block text-primary-600">Guitton</span>
+          </span>
+          <span className="block text-sm font-semibold text-gray-500">
+            {/* for SEO we need a space here */}
+            <span className="hidden">&nbsp;-&nbsp;</span>
+            Tech Lead NFT and Principal Machine Learning Engineer at{" "}
+            <b>OneFootball</b>
+          </span>
+        </h1>
+        <p className="mt-3 text-sm text-gray-500 sm:mt-5">
+          Building for myself first, for fun and learnings. Experienced with
+          Natural Language Processing, Recommenders Systems, MVP Building and
+          the Flow blockchain.
+        </p>
+      </div>
+      <button
+        className={clsx(
+          "relative mt-12 sm:mt-0 sm:col-span-6 sm:flex sm:items-center",
+          "rounded-full shadow-lg h-56 w-56",
+          "mx-auto",
+          "focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary-500"
+        )}>
+        <Image
+          layout="fill"
+          src="/images/louis.jpg"
+          className="rounded-full"
+          alt="Profile picture"
+        />
+      </button>
+    </section>
   );
 }
 
